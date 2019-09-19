a1a38c8acf813dd4bf1a5ca90c6dc39ddca6927f
hongfangj <55541263+hongfangj@users.noreply.github.com>
2019-09-19 22:34:22 +1000

    Update nhs.sql

--- nhs.sql
+++ nhs.sql
@@ -40,3 +40,28 @@ CREATE TABLE [GRN\EJing].[Data_Item](
 
 GO
 
+
+
+USE [Actuarial]
+GO
+
+/****** Object:  Table [GRN\EJing].[Unit_Record]    Script Date: 19/09/2019 10:32:18 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [GRN\EJing].[Unit_Record](
+	[id] [int] NOT NULL,
+	[Record_number] [int] NOT NULL,
+	[SAS_id] [int] NOT NULL,
+	[Data_Item_id] [int] NOT NULL,
+ CONSTRAINT [PK_Unit_Record] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
